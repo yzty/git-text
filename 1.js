36a7aac7df4aba8f0ebfa05ec51715069a3c85d4
yzty <2390512763@qq.com>
2019-06-26 10:20:17 +0800

    afad

--- 1.js
+++ 1.js
@@ -1,9 +1,10 @@
 var a=12;
 var b=15;
 var c=a+b;
+master add
 
 ======================
-dev
+master
 	var allList = initArr(treeList.data, res = [])
 	var allList = initArr(treeList.data, res = [])
 	121212
